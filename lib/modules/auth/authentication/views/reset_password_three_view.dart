32a8aa88e9071c4ae5a1a2d837b2d3cbcc049b65
mdbayzid131 <bayazidbayazid567@gmail.com>
2026-08-26 13:07:16 +0600

    feat: implement reset password request and update password views

--- lib/modules/auth/authentication/views/reset_password_three_view.dart
+++ lib/modules/auth/authentication/views/reset_password_three_view.dart
@@ -7,6 +7,7 @@ import '../../../../core/widgets/CustomButton.dart';
 import '../../../../core/widgets/CustomText.dart';
 import '../../../../core/widgets/CustomTextField.dart';
 import '../../../../core/widgets/CustomTextGary.dart';
+import '../../../../core/widgets/custom_sub_appbar.dart';
 import '../controllers/reset_password_controller.dart';
 
 class ResetPasswordThreeView extends GetView<ResetPasswordController> {
@@ -14,45 +15,74 @@ class ResetPasswordThreeView extends GetView<ResetPasswordController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: controller.formKey,
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.w),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: const CustomSubAppBar(
+          title: "Reset Password",
+        ),
+        body: SafeArea(
+          child: Form(
+            key: controller.formKey,
             child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 24.h),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Align(
-                    alignment: Alignment.centerLeft,
-                    child: CustomText(text: "Forget Password"),
+                  Center(
+                    child: Container(
+                      width: 80.w,
+                      height: 80.w,
+                      margin: EdgeInsets.only(top: 10.h, bottom: 24.h),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF161616),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: const Color(0xFF2C2C2C),
+                          width: 1.5,
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.lock_outline_rounded,
+                        color: const Color(0xFFFFDCA1),
+                        size: 38.sp,
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 30.h),
+
+                  CustomText(
+                    text: "Create New Password",
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  SizedBox(height: 8.h),
                   CustomTextgray(
                     text:
-                        "Enter your email address and we'll send you a link to reset your password.",
-                    fontSize: 15,
+                        "Your new password must be at least 8 characters long with uppercase, digits, and special characters.",
+                    fontSize: 14.sp,
                     fontWeight: FontWeight.w400,
                   ),
-                  SizedBox(height: 26.h),
+                  SizedBox(height: 28.h),
 
-                  // ── New Password ──
+                  CustomText(
+                    text: "New Password",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.sp,
+                  ),
+                  SizedBox(height: 8.h),
                   Obx(
                     () => Customtextfield(
                       controller: controller.passwordController,
-                      hintText: "New Password",
+                      hintText: "Enter new password",
                       obscureText: !controller.isPasswordVisible.value,
                       textInputType: TextInputType.visiblePassword,
-                      validator: (value) => Validators.password(
-                        value,
-                        minLength: 8,
-                        requireDigit: true,
-                        requireUppercase: true,
-                        requireSpecialChar: true,
+                      prefixIcon: Icon(
+                        Icons.lock_outline_rounded,
+                        color: AppColors.gray100,
+                        size: 20.sp,
                       ),
-                      prefixIcon: IconButton(
+                      suffixIcon: IconButton(
                         icon: Icon(
                           controller.isPasswordVisible.value
                               ? Icons.visibility_outlined
@@ -61,17 +91,43 @@ class ResetPasswordThreeView extends GetView<ResetPasswordController> {
                         ),
                         onPressed: controller.togglePassword,
                       ),
+                      validator: (value) => Validators.password(
+                        value,
+                        minLength: 8,
+                        requireDigit: true,
+                        requireUppercase: true,
+                        requireSpecialChar: true,
+                      ),
                     ),
                   ),
-                  SizedBox(height: 26.h),
+                  SizedBox(height: 20.h),
 
-                  // ── Confirm Password ──
+                  CustomText(
+                    text: "Confirm New Password",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.sp,
+                  ),
+                  SizedBox(height: 8.h),
                   Obx(
                     () => Customtextfield(
                       controller: controller.confirmPasswordController,
-                      hintText: "Confirm New Password",
+                      hintText: "Re-enter new password",
                       obscureText: !controller.isPasswordVisibleTwo.value,
                       textInputType: TextInputType.visiblePassword,
+                      prefixIcon: Icon(
+                        Icons.lock_outline_rounded,
+                        color: AppColors.gray100,
+                        size: 20.sp,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          controller.isPasswordVisibleTwo.value
+                              ? Icons.visibility_outlined
+                              : Icons.visibility_off_outlined,
+                          color: AppColors.gray100,
+                        ),
+                        onPressed: controller.toggleConfirmPassword,
+                      ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
                           return "Enter Confirm New Password";
@@ -81,33 +137,19 @@ class ResetPasswordThreeView extends GetView<ResetPasswordController> {
                         }
                         return null;
                       },
-                      prefixIcon: IconButton(
-                        icon: Icon(
-                          controller.isPasswordVisibleTwo.value
-                              ? Icons.visibility_outlined
-                              : Icons.visibility_off_outlined,
-                          color: AppColors.gray100,
-                        ),
-                        onPressed: controller.toggleConfirmPassword,
-                      ),
                     ),
                   ),
-                  SizedBox(height: 50.h),
+                  SizedBox(height: 36.h),
 
-                  // ── Reset Button ──
                   Obx(
                     () => CustomButton(
-                      text: controller.isLoading.value
-                          ? "Resetting..."
-                          : "Reset Password",
+                      loading: controller.isLoading.value,
+                      text: "Reset Password",
                       onPressed: () {
                         if (!controller.isLoading.value) {
                           controller.resetPassword();
                         }
                       },
-                      backgroundColor: Colors.white,
-                      textColor: Colors.black,
-                      borderColor: Colors.white,
                     ),
                   ),
                 ],
--- lib/modules/auth/authentication/views/reset_password_view.dart
+++ lib/modules/auth/authentication/views/reset_password_view.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:moeb_26/config/constants/image_paths.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:moeb_26/config/routes/app_pages.dart';
+import 'package:moeb_26/config/themes/app_theme.dart';
 import 'package:moeb_26/core/utils/app_const.dart';
 import '../../../../core/widgets/CustomButton.dart';
 import '../../../../core/widgets/CustomText.dart';
+import '../../../../core/widgets/CustomTextField.dart';
 import '../../../../core/widgets/CustomTextGary.dart';
+import '../../../../core/widgets/custom_sub_appbar.dart';
 import '../controllers/forget_password_controller.dart';
 
 class ResetPasswordView extends GetView<ForgotPasswordController> {
@@ -14,134 +17,118 @@ class ResetPasswordView extends GetView<ForgotPasswordController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: controller.formKey,
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 20.w),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: const CustomSubAppBar(
+          title: "Forgot Password",
+        ),
+        body: SafeArea(
+          child: Form(
+            key: controller.formKey,
             child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 24.h),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.asset(AppImages.app_logo, width: 100.w, height: 100.h),
-                  SizedBox(height: 20.h),
-                  CustomText(text: "Elite Chauffeur Network"),
-                  SizedBox(height: 30.h),
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 20.w,
-                      vertical: 20.w,
+                  Center(
+                    child: Container(
+                      width: 80.w,
+                      height: 80.w,
+                      margin: EdgeInsets.only(top: 10.h, bottom: 24.h),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF161616),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: const Color(0xFF2C2C2C),
+                          width: 1.5,
+                        ),
+                      ),
+                      child: Icon(
+                        Icons.lock_reset_rounded,
+                        color: const Color(0xFFFFDCA1),
+                        size: 38.sp,
+                      ),
                     ),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF394156),
-                      border: Border.all(color: Color(0xFF1E2939)),
-                      borderRadius: BorderRadius.circular(10.r),
+                  ),
+
+                  CustomText(
+                    text: "Forgot Password?",
+                    fontSize: 22.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  SizedBox(height: 8.h),
+                  CustomTextgray(
+                    text:
+                        "Enter your registered email address to receive a 6-digit verification code to reset your password.",
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  SizedBox(height: 28.h),
+
+                  CustomText(
+                    text: "Email Address",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.sp,
+                  ),
+                  SizedBox(height: 8.h),
+                  Customtextfield(
+                    controller: controller.emailController,
+                    hintText: "your.email@example.com",
+                    obscureText: false,
+                    textInputType: TextInputType.emailAddress,
+                    prefixIcon: Icon(
+                      Icons.mail_outline_rounded,
+                      color: AppColors.gray100,
+                      size: 20.sp,
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              onPressed: () => Get.back(),
-                              icon: Icon(Icons.arrow_back, color: Colors.white),
-                            ),
-                            SizedBox(width: 10.w),
-                            CustomText(text: "Forget Password", fontSize: 20),
-                          ],
-                        ),
-                        SizedBox(width: 10.w),
-                        CustomTextgray(
-                          text:
-                              "Enter your email address and we'll send you a link to reset your password.",
-                          color: Color(0xFFE3DCDC),
-                        ),
-                        SizedBox(height: 20.h),
-                        CustomText(
-                          text: "Email Address",
-                          fontWeight: FontWeight.w500,
-                          fontSize: 14,
-                        ),
-                        SizedBox(height: 8.h),
-                        TextFormField(
-                          controller: controller.emailController,
-                          obscureText: false,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Enter your Email";
-                            }
-                            if (!AppString.emailRegexp.hasMatch(value)) {
-                              return "Invalid Email";
-                            }
-                            return null;
-                          },
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            hintText: "your.email@example.com",
-                            hintStyle: TextStyle(color: Color(0xFFE3DCDC)),
-                            contentPadding: EdgeInsets.symmetric(
-                              vertical: 15.h,
-                              horizontal: 10.w,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.r),
-                              borderSide: BorderSide(color: Color(0xFFE3DCDC)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.r),
-                              borderSide: BorderSide(color: Color(0xFFE3DCDC)),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(16.r),
-                              borderSide: BorderSide(color: Color(0xFFE3DCDC)),
-                            ),
+                    validator: (value) {
+                      if (value == null || value.trim().isEmpty) {
+                        return "Enter your email";
+                      }
+                      if (!AppString.emailRegexp.hasMatch(value.trim())) {
+                        return "Enter a valid email address";
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 32.h),
+
+                  Obx(
+                    () => CustomButton(
+                      loading: controller.isLoading.value,
+                      text: "Send Verification Code",
+                      onPressed: () {
+                        if (!controller.isLoading.value) {
+                          controller.forgotPassword();
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 24.h),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CustomTextgray(
+                        text: "Remember your password?",
+                        fontSize: 14.sp,
+                        color: AppColors.gray100,
+                      ),
+                      SizedBox(width: 6.w),
+                      GestureDetector(
+                        onTap: () => Get.offNamed(Routes.signinView),
+                        child: Text(
+                          "Sign In",
+                          style: GoogleFonts.inter(
+                            color: const Color(0xFFFFDCA1),
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w600,
                           ),
                         ),
-                        SizedBox(height: 20.h),
-
-                        // ── Send Button ──
-                        Obx(
-                          () => CustomButton(
-                            text: controller.isLoading.value
-                                ? "Sending..."
-                                : "Send Reset Link",
-                            onPressed: () {
-                              if (!controller.isLoading.value) {
-                                controller.forgotPassword();
-                              }
-                            },
-                            backgroundColor: Colors.white,
-                            textColor: Colors.black,
-                            borderColor: Colors.white,
-                          ),
-                        ),
-
-                        SizedBox(height: 20.h),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            CustomTextgray(
-                              text: "Remember your password?",
-                              color: Color(0xFFE3DCDC),
-                            ),
-                            SizedBox(width: 10.w),
-                            GestureDetector(
-                              onTap: () => Get.toNamed(Routes.signinView),
-                              child: CustomTextgray(
-                                text: "Sign In",
-                                color: Color(0xFFA49898),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ],
               ),
